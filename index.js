58a4dafe6f84310d8b2b87958303d47b016e5858
KoczILak <skarbnikkrystian@gmail.com>
2026-07-18 10:49:24 +0200

    added "Kliknij." Window.

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ document.addEventListener('click', function() {
     var audio = document.getElementById('bg-music');
     audio.muted = false;
     audio.play();
+
+    var tekstPrzedDIV = document.querySelector('.tekstPrzedDIV');
+
+    if (tekstPrzedDIV) {
+        tekstPrzedDIV.style.opacity = 0;
+    }
 }, { once: true });
